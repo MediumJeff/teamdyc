2ec286fc914cec160882ee8f8a6c13d001edfb7f
Jeff McCracken <jjmccrac@gmail.com>
2023-06-12 13:36:57 -0700

    Setting up nav, figuring out vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home.jsx';
 import NavBar from './components/NavBar';
 
 function App() {
 
   return (
-    <div className='App'>
-      <NavBar />
-    </div>
+  <div className='App'>
+    <NavBar />
+    <Home />
+  </div>
   )
+
 }
 
 export default App
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,49 +3,15 @@ import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem } from 'reac
 import { NavLink } from 'react-router-dom';
 
 
-const NavBar = () => {
+export default function NavBar() {
 
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className='navbar'>
-      <Navbar>
-        <NavbarBrand href='/' >
-          <img src={Logo} alt='RR logo' className='logo float-start'></img>
-          <h1 className='col-12'>Route Runners</h1>
-        </NavbarBrand>
-
-        <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} className='col-12' />
-        <Collapse isOpen={menuOpen} navbar>
-          <Nav className='ms-auto' navbar>
-            <NavItem>
-              <NavLink className='nav-link text-center' aria-label='Link to home page' to='/'>
-                <i className='fa fa-home fa-lg' /> Home
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className='nav-link text-center' aria-label='Link to services page' to='/services'>
-                <i className='fa fa-truck fa-lg' /> Services
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className='nav-link text-center' aria-label='Link to about us page' to='/about'>
-                <i className='fa fa-list fa-lg' /> About
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className='nav-link text-center' aria-label='Link to contact form' to='/contact'>
-                <i className='fa fa-address-card fa-lg' /> Contact
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className='nav-link' to='/' aria-label='Link back to home page'></NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </header>
+    <div className='navbar'>
+      <header>
+        <h1>NavBar</h1>
+      </header>
+     </div>
   )
 };
-
-export default NavBar;
